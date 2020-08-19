86cf910797215b4c74965408416ca97fdae5de87
MMcClure11 <mcclme02@luther.edu>
2020-08-19 11:28:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(cards, event) {
+  let newCards = []
+  for (let i = 0; i < cards.length; i++) {
+    newCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+  }
+  return newCards;
+}
+
+function countDown(num) {
+  while (num > 0) {
+    console.log(num);
+    num--;
+  }
+  console.log(num)
+}
