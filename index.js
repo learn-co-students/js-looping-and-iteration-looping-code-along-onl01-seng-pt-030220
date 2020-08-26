f273d1b88f157d4d5dac1bca2dc843567e9fbd30
Austin Redmond <aredmond85@gmail.com>
2020-08-26 21:30:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
 function writeCards( namesArray, eventName ) {
-  let thankYouCards = []
+  let cards = []
   for ( let i = 0; i < namesArray.length; i++ ) {
-    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!` )
+    cards.push( `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!` )
   }
-  return thankYouCards
+  return cards
 }
 
 
