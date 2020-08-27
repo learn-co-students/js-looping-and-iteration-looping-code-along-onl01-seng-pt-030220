9a201b16faa6ba822fd33c13eb8227c1986800bf
Rkebede <rutht.kebede@gmail.com>
2020-08-27 11:44:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  const thankYouCards = []
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return thankYouCards; 
+}
+
+function countDown(number) {
+  
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
