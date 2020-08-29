c2ade253e98dfcc45239b22a6c4578e09828dc2b
github username <github email address>
2020-08-29 14:21:34 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const array = ["Ada", "Brendan", "Ali"] 
+const event = 'birthday'
+
+const messages = []
+
+function writeCards(array, event){ 
+    for (let i = 0 ; i < array.length ; i++ ) { 
+    const meow = (`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    console.log(meow)
+    messages.push(meow)
+    }
+    return messages
+}
+
+
+function countDown(number){
+while (number >= 0){
+    console.log(number)
+    number--
+}
+}
