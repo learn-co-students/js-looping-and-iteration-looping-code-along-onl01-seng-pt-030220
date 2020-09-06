d6b30b03037c7e2d6334e49c2fd93059f203b90d
Haloren <mckennakyler@gmail.com>
2020-09-05 23:51:56 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arrayNames, event) {
+    const customMessage = [];
+    for (let i = 0; i < arrayNames.length; i++) {
+        customMessage.push(`Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!`);
+    };
+    return customMessage;
+};
+
+function countDown(num) {
+    for (let i = num; i >=0; i--) {
+        console.log(i);
+    };
+};
