a321bb6c6f8bfdf007af56f552034280bcbb983c
German Cruz <cruzgerman216@yahoo.com>
2020-09-15 07:29:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr, element){
+  for(let i = 0; i<arr.length;i++){
+    arr[i] = "Thank you, " + arr[i] + ", for the wonderful " + element + " gift!";
+  }
+  return arr;
+}
+
+function countDown(num){
+  while(num > -1){
+    console.log(num)
+    num--;
+  }
+}
